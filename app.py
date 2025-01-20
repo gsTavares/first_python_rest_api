c91cfa9edd4b7e971877998d250432e47f300070
Gustavo Tavares <gustavo.tavares.dev@outlook.com>
2025-01-20 20:13:59 -0300

    feat: added more API endpoints

--- app.py
+++ app.py
@@ -17,13 +17,6 @@ def get_store(store_id):
     except KeyError:
         abort(404, message="Store not found!")
 
-@app.get("/item/<string:item_id>")
-def get_item(item_id):
-    try:
-        return {"item": items[item_id]}, 200
-    except KeyError:
-        abort(404, message="Item not found!")
-
 @app.post("/store")
 def create_store():
     store_data = request.get_json()
@@ -39,6 +32,14 @@ def create_store():
     store = {**store_data, "id": store_id}
     stores[store_id] = store
     return store, 201
+
+@app.delete("/store/<string:store_id>")
+def delete_store(store_id):
+    try:
+        del stores[store_id]
+        return {"message", "Store deleted!"}
+    except KeyError:
+        abort(404, message="Store not found!")
 
 @app.post("/item")
 def create_item():
@@ -63,3 +64,35 @@ def create_item():
 @app.get("/item")
 def get_items():
     return {"items": list(items.values())}
+
+@app.get("/item/<string:item_id>")
+def get_item(item_id):
+    try:
+        return {"item": items[item_id]}, 200
+    except KeyError:
+        abort(404, message="Item not found!")
+
+@app.delete("/item/<string:item_id>")
+def delete_item(item_id):
+    try:
+        del items[item_id]
+        return {"message", "Item deleted!"}
+    except KeyError:
+        abort(404, message="Item not found!")
+
+@app.put("/item/<string:item_id>")
+def update_item(item_id):
+    item_data = request.get_json()
+    if "price" not in item_data or "name" not in item_data:
+        abort(400, message="Bad request. Ensure 'price' and 'name' are included in the JSON payload.")
+    
+    try:
+        item = items[item_id]
+        item |= item_data # in-place modification
+
+        return {"item": item}, 200
+    except KeyError:
+        abort(404, message="Item not found!")
+
+
+
