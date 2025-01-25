42ab3c112933a1d381ece1c589f1a8691cedf441
Gustavo Tavares <gustavo.tavares.dev@outlook.com>
2025-01-25 16:19:07 -0300

    feat: added sql-alchemy models

--- resources/store.py
+++ resources/store.py
@@ -3,14 +3,14 @@ from flask import request
 from flask.views import MethodView
 from flask_smorest import Blueprint, abort
 from db import stores
-from schemas import StoreSchema
+from schemas import PlainStoreSchema
 
 blp = Blueprint("stores", __name__, description="Operations on stores")
 
 @blp.route("/store/<string:store_id>")
 class Store(MethodView):
 
-    @blp.response(200, StoreSchema)
+    @blp.response(200, PlainStoreSchema)
     def get(self, store_id):
         try:
             return stores[store_id]
@@ -27,12 +27,12 @@ class Store(MethodView):
 @blp.route("/store")        
 class StoreList(MethodView):
 
-    @blp.response(200, StoreSchema(many=True))
+    @blp.response(200, PlainStoreSchema(many=True))
     def get(self):
         return stores.values()
     
-    @blp.arguments(StoreSchema)
-    @blp.response(201, StoreSchema)
+    @blp.arguments(PlainStoreSchema)
+    @blp.response(201, PlainStoreSchema)
     def post(self, store_data):
         for store in stores.values():
             if store_data["name"] == store["name"]:
