4102eaf47771603d9d8a21cb5d2d8d4856467d07
Gustavo Tavares <gustavo.tavares.dev@outlook.com>
2025-01-28 22:51:54 -0300

    feat: error handling and jwt claims

--- app.py
+++ app.py
@@ -1,7 +1,6 @@
 import os
-import secrets
 
-from flask import Flask
+from flask import Flask, jsonify
 from flask_smorest import Api
 from flask_jwt_extended import JWTManager
 
@@ -32,6 +31,33 @@ def create_app(db_url=None):
 
     app.config["JWT_SECRET_KEY"] = "202777794991377142149168373565702516904"
     jwt = JWTManager(app)
+
+    @jwt.additional_claims_loader
+    def add_claims_to_jwt(identity):
+        if identity == "1": # non-real example
+            return {"is_admin": True}
+        return {"is_admin": False}
+
+    @jwt.expired_token_loader
+    def expired_token_callback(jwt_header, jwt_payload):
+        return (
+            jsonify({"message": "The token has expired.", "error": "token_expired"}),
+            401
+        )
+    
+    @jwt.invalid_token_loader
+    def invalid_token_callback(error):
+        return (
+            jsonify({"message": "Signature verification failed", "error": "invalid_token"}),
+            401
+        )
+    
+    @jwt.unauthorized_loader
+    def missing_token_callback(error):
+        return (
+            jsonify({"message": "Request does not contain an access token.", "error": "authorization_required"}),
+            401
+        )
 
     with app.app_context():
         db.create_all()
