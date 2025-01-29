a8afd95dc6b37db602a798f11a11fd926359a17a
Gustavo Tavares <gustavo.tavares.dev@outlook.com>
2025-01-28 23:59:20 -0300

    feat: migrations with flask-migrate

--- app.py
+++ app.py
@@ -3,6 +3,7 @@ import os
 from flask import Flask, jsonify
 from flask_smorest import Api
 from flask_jwt_extended import JWTManager
+from flask_migrate import Migrate
 
 from db import db
 import models
@@ -26,6 +27,7 @@ def create_app(db_url=None):
     app.config["SQLALCHEMY_DATABASE_URI"] = db_url or os.getenv("DATABASE_URL", "sqlite:///data.db")
     app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
     db.init_app(app)
+    migrate = Migrate(app, db)
 
     api = Api(app)
 
@@ -64,9 +66,6 @@ def create_app(db_url=None):
             jsonify({"message": "Request does not contain an access token.", "error": "authorization_required"}),
             401
         )
-
-    with app.app_context():
-        db.create_all()
 
     api.register_blueprint(ItemBlueprint)
     api.register_blueprint(StoreBlueprint)
